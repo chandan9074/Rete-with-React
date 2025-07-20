5c59ade82d0534fa342c1ded7d633548f3222e61
chandan-zaag <chandan@zaagsys.com>
2025-07-20 12:46:22 +0600

    feat: implement Webhook component with context menu actions and update NodeWrapper to use Webhook

--- src/NodeWrapper.jsx
+++ src/NodeWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nodes from "./components/Nodes";
 
 const NodeWrapper = (props) => {
-    return <Nodes.Code {...props} />;
+    return <Nodes.Webhook {...props} />;
 };
 
 export default NodeWrapper;
--- src/components/Nodes/index.js
+++ src/components/Nodes/index.js
@@ -2,12 +2,14 @@ import { ManualTrigger } from "./ManualTrigger";
 import { ScheduleTrigger } from "./ScheduleTrigger";
 import { HttpRequest } from "./HttpRequest";
 import { Code } from "./Code";
+import { Webhook } from "./Webhook";
 
 const Nodes = {
     ManualTrigger,
     ScheduleTrigger,
     HttpRequest,
     Code,
+    Webhook,
 };
 
 export default Nodes;
