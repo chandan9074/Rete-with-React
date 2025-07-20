10bcc98ad44d859c046c143fa104c27d6d93430e
chandan-zaag <chandan@zaagsys.com>
2025-07-20 12:40:52 +0600

    feat: add Code component with context menu actions and update NodeWrapper to use Code

--- src/NodeWrapper.jsx
+++ src/NodeWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nodes from "./components/Nodes";
 
 const NodeWrapper = (props) => {
-    return <Nodes.HttpRequest {...props} />;
+    return <Nodes.Code {...props} />;
 };
 
 export default NodeWrapper;
--- src/components/Nodes/index.js
+++ src/components/Nodes/index.js
@@ -1,11 +1,13 @@
 import { ManualTrigger } from "./ManualTrigger";
 import { ScheduleTrigger } from "./ScheduleTrigger";
 import { HttpRequest } from "./HttpRequest";
+import { Code } from "./Code";
 
 const Nodes = {
     ManualTrigger,
     ScheduleTrigger,
     HttpRequest,
+    Code,
 };
 
 export default Nodes;
